ce3f2c74d1dfa03af891c5388741d99ee6c3f5b7
Ingplazas <61364729+Ingplazas@users.noreply.github.com>
2021-11-24 00:43:42 -0300

    Pantalla Inicio Sesion

--- app/src/main/java/com/example/entubarrioapp/Start_Session.java
+++ app/src/main/java/com/example/entubarrioapp/Start_Session.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Start_Session extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Start_Session);
     }
 }
---
+++ app/src/main/java/com/example/entubarrioapp/Tipo_Usuario.java
@@ -0,0 +1,14 @@
+package com.example.entubarrioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tipo_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Tipo_Usuario);
+    }
+}
