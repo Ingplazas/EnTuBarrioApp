477004592727ebdd602cfb567e5e7a858904c808
Juan Puerto <sebaspuerto77@gmail.com>
2021-11-26 16:15:52 -0500

    Intento de arreglar registro CLientes 1

--- app/src/main/java/com/example/entubarrioapp/MainActivity4.java
+++ app/src/main/java/com/example/entubarrioapp/MainActivity4.java
@@ -12,14 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity4 extends AppCompatActivity {
 
-    private EditText etNombresC, etDireccionC, etTelefonoC, etEmailC, etPasswordC;
+    private EditText etNombreC, etDireccionC, etTelefonoC, etEmailC, etPasswordC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        etNombresC = findViewById(R.id.et_NombreCliente);
+        etNombreC = findViewById(R.id.et_NombreCliente);
         etDireccionC = findViewById(R.id.et_DireccionCliente);
         etTelefonoC = findViewById(R.id.et_TelefonoCliente);
         etEmailC = findViewById(R.id.et_EmailCliente);
@@ -31,16 +31,16 @@ public class MainActivity4 extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "enTuBarrio", null, 1);
         SQLiteDatabase db = admin.getWritableDatabase();
 
-        String nombresC = etNombresC.getText().toString();
-        String emailC = etEmailC.getText().toString();
+        String nombreC = etNombreC.getText().toString();
         String direccionC = etDireccionC.getText().toString();
         String telefonoC = etTelefonoC.getText().toString();
+        String emailC = etEmailC.getText().toString();
         String passwordC = etPasswordC.getText().toString();
 
-        if (!nombresC.isEmpty() && !direccionC.isEmpty() && !telefonoC.isEmpty() && !emailC.isEmpty() && !passwordC.isEmpty()) {
+        if (!nombreC.isEmpty() && !direccionC.isEmpty() && !telefonoC.isEmpty() && !emailC.isEmpty() && !passwordC.isEmpty()) {
             ContentValues registro = new ContentValues();
 
-            registro.put("nombreC", nombresC);
+            registro.put("nombreC", nombreC);
             registro.put("direccionC", direccionC);
             registro.put("telefonoC", telefonoC);
             registro.put("emailC", emailC);
@@ -49,7 +49,7 @@ public class MainActivity4 extends AppCompatActivity {
             db.insert("Cliente", null, registro);
 
             db.close();
-            etNombresC.setText("");
+            etNombreC.setText("");
             etDireccionC.setText("");
             etTelefonoC.setText("");
             etEmailC.setText("");
